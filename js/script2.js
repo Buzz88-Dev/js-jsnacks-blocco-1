8d12d199690db80586b1ac6815356e82f370bae1
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-05-10 16:27:10 +0200

    ended

--- js/script2.js
+++ js/script2.js
@@ -15,3 +15,25 @@ for (let i = 0; i < 10; i++){
 output.innerHTML = "I 10 numeri dell'array sono: " + arrayNumb;
 
 console.log(arrayNumb);
+
+
+// crea un array che parte vuoto ma viene riempito automaticamente di 10 numeri casuali da 1 a 50 generati dal pc
+// possono esserci anche doppioni
+
+let arrayNumbTwo = [];
+console.log(arrayNumbTwo);
+
+let output2 = document.getElementById("myDiv2");
+
+for (let i = 0; i < 10; i++){
+    let numPcTwo = Math.floor(Math.random() * 50) + 1;
+
+    while (arrayNumbTwo.includes(numPcTwo)) 
+    { numPcTwo = Math.floor(Math.random() * 50) + 1; }
+
+    arrayNumbTwo.push(numPcTwo);
+}
+
+output2.innerHTML = "I 10 numeri diversi dell'array sono: " + arrayNumbTwo;
+
+console.log(arrayNumbTwo);
